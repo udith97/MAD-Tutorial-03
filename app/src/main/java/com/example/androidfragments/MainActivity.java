d1dc6957e0faf9ec9d05133df8f7d20fbf2b373a
udith <udithmadushan2@gmail.com>
2020-03-21 13:26:02 +0530

    Second Commit

--- app/src/main/java/com/example/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/androidfragments/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.androidfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view) {
+        Fragment fragment;
+
+        if (view == findViewById(R.id.fragment1)) {
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault, fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.fragment2)) {
+            fragment = new Fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault, fragment);
+            ft.commit();
+        }
+
     }
 }
